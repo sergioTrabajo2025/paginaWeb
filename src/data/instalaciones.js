09c83ce087ae69937f98728835ca7270be9bac08
sergioTrabajo2025 <cuentasergiotrabajo@gmail.com>
2025-12-05 21:45:19 -0300

    act imagenes

--- src/data/instalaciones.js
+++ src/data/instalaciones.js
@@ -2,34 +2,44 @@
 export const INSTALACIONES = [
   {
     src: "/gallery/instalaciones/inst_01.jpg",
-    alt: "Rack con controlador y módem en gabinete",
-    caption: "Instalación LGR el día xx/xx/xx",
+    alt: "Charruas instalación",
+    caption: "LGR Charruas: Instalación LGR el día 05/09/24",
   },
   {
     src: "/gallery/instalaciones/inst_02.jpg",
-    alt: "Vista de sensores conectados",
-    caption: "Instalación LBS el día xx/xx/xx",
+    alt: "Bornera SMB",
+    caption: "LGR Charruas: Borneras auxiliares",
   },
    {
     src: "/gallery/instalaciones/inst_03.jpg",
-    alt: "Vista de sensores conectados",
-    caption: "Instalación LBS el día xx/xx/xx",
+    alt: "LGR Charruas: vista de atras de los armarios ",
+    caption: "LGR Charruas: Vista atras del armario",
   },
    {
     src: "/gallery/instalaciones/inst_04.jpg",
-    alt: "Vista de sensores conectados",
-    caption: "Instalación LBS el día xx/xx/xx",
+    alt: "LGR Charruas: vista de frente del armario",
+    caption: "LGR Charruas: vista de frente del armario",
   },
    {
     src: "/gallery/instalaciones/inst_05.jpg",
-    alt: "Vista de sensores conectados",
-    caption: "Instalación LBS el día xx/xx/xx",
+    alt: "Instalación",
+    caption: "LBS Gorriti-G: Instalación LBS el día 30/05/25",
   },
    {
     src: "/gallery/instalaciones/inst_06.jpg",
-    alt: "Vista de sensores conectados",
-    caption: "Instalación LBS el día xx/xx/xx",
+    alt: "LBS Gorriti-G: armario",
+    caption: "LBS Gorriti-G: Armario ",
   },
-  
+  {
+    src: "/gallery/instalaciones/inst_07.jpg",
+    alt: "Instalación antena",
+    caption: "LBS Gorriti-G: Intalación de la antena",
+  },
+  {
+    src: "/gallery/instalaciones/inst_08.jpg",
+    alt: "interior armario 157",
+
+    caption: "LBS Gorriti-G: Vista del interior del armario 157",
+  },
   // podés agregar más...
 ];
